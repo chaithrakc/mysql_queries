32762c007db7c3ab6f351c0166179340b2a7889e
Chaithra K C <chaithra.kc22@gmail.com>
2021-09-25 23:29:36 -0400

    select query

---
+++ basic_select/revising_the_select_query1.sql
@@ -0,0 +1,20 @@
+/* 
+DIFFUCULTY: EASY
+
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
++-------------+--------------+
+|    Field    |     Type     |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+*/
+
+select * from city where countrycode = 'USA' and population > 100000;
+
