73c43de0db53143643d70545e70c8aacde9e4dfb
Chaithra K C <chaithra.kc22@gmail.com>
2021-12-29 04:18:08 -0500

    placements solution

--- hackerrank/05advanced_join/p02placements.sql
+++ hackerrank/05advanced_join/p02placements.sql
@@ -4,3 +4,23 @@ https://www.hackerrank.com/challenges/placements/problem
 
 */
 
+select s.name
+from students as s inner join packages p on s.id=p.id
+inner join friends as f on s.id=f.id
+inner join packages as fp on f.friend_id=fp.id
+where p.salary < fp.salary
+order by fp.salary;
+
+/* output
+Stuart 
+Priyanka 
+Paige 
+Jane 
+Julia 
+Belvet 
+Amina 
+Kristeen 
+Scarlet 
+Priya 
+Meera
+*/
