be5ba2a9871eb6e35974cff19d5535917afd712e
Chaithra K C <chaithra.kc22@gmail.com>
2023-04-25 22:00:17 -0400

    updating the output

--- leetcode/easy/p1179reformat_department_table.sql
+++ leetcode/easy/p1179reformat_department_table.sql
@@ -149,6 +149,35 @@ select id,
 "Nov" as "Nov_Revenue", 
 "Dec" as "Dec_Revenue" 
 from pivot_source PIVOT(
-    sum(revenue) for "month" in ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-    ) as pivot_table
+    sum(revenue) for "month" in ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) as pivot_table
+order by id
+
+-- without common table expression also works
+select id, 
+"Jan" as "Jan_Revenue", 
+"Feb" as "Feb_Revenue", 
+"Mar" as "Mar_Revenue", 
+"Apr" as "Apr_Revenue", 
+"May" as "May_Revenue", 
+"Jun" as "Jun_Revenue", 
+"Jul" as "Jul_Revenue", 
+"Aug" as "Aug_Revenue", 
+"Sep" as "Sep_Revenue", 
+"Oct" as "Oct_Revenue", 
+"Nov" as "Nov_Revenue", 
+"Dec" as "Dec_Revenue" 
+from department PIVOT(
+    sum(revenue) for "month" in ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) as pivot_table
+order by id
+
+/*
+OUTPUT:
+
+| id | Jan_Revenue | Feb_Revenue | Mar_Revenue | Apr_Revenue | May_Revenue | Jun_Revenue | Jul_Revenue | Aug_Revenue | Sep_Revenue | Oct_Revenue | Nov_Revenue | Dec_Revenue |
+| -- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- |
+| 1  | 8000        | 7000        | 6000        | null        | null        | null        | null        | null        | null        | null        | null        | null        |
+| 2  | 9000        | null        | null        | null        | null        | null        | null        | null        | null        | null        | null        | null        |
+| 3  | null        | 10000       | null        | null        | null        | null        | null        | null        | null        | null        | null        | null        |
+*/
+
 
