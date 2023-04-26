f8ddab060a657c748114c53ee3810881f7200a99
Chaithra K C <chaithra.kc22@gmail.com>
2023-04-26 14:58:45 -0400

    optimizing the query

--- leetcode/medium/p0184department_highest_salary.sql
+++ leetcode/medium/p0184department_highest_salary.sql
@@ -70,6 +70,17 @@ highest salary in the Sales department.
 
 */
 
+-- using common table expression and correlated sub-query
+with dept_max_sal as( 
+    select departmentId, max(salary) as highest_sal 
+    from employee 
+    group by departmentId
+    )
+select d.name as Department, e.name as Employee, e.salary as Salary
+from employee e inner join department d on e.departmentId = d.id
+where e.salary = (select highest_sal from dept_max_sal c where e.departmentId = c.departmentId)
+
+-- without CTE
 select dept.name as "Department", emp.name as "Employee", emp.salary as "Salary"
 from employee emp inner join department dept on emp.departmentId = dept.id
 where (emp.departmentId, emp.salary) in (select departmentId, max(salary)
